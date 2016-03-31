53fdd3a423c71697201656096a1d646e306c8a0d
fuanka <fuankarion@gmail.com>
2016-03-30 23:17:50 -0500

    para hacer merge

--- lab6_textons/example.m
+++ lab6_textons/example.m
@@ -22,20 +22,21 @@ filterSize=size(fb{1,1});
 % size(fbCol1)
 % size(fbCol2)
 % imshow(mat2gray(fbCol1));
-% imshow(mat2gray(fbCol2));
+% imshow(mat2gray(fbCol2/usr/local/matlab/R2015a/bin/matlab –nodisplay
 % drawnow;
 
 % numero de textones en diccionario
-km = 20;
+km = 50;
 
-D=dir('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/');
+D=dir('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/train/');
 
-for i=3:numel(D)
-    singleTextureFullTestSet=double(zeros(0,0));
+singleTextureFullTestSet=double(zeros(0,0));
+for i=3:numel(D)    
     D(i).name
-    F=dir(strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/',D(i).name,'/*.jpg'));
-    for j=1:numel(F)
-        name=strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/',D(i).name,'/',F(j).name)
+    F=dir(strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/train/',D(i).name,'/*.jpg'));
+    randomImgIdx=round(rand(1,3)*29)
+    for j=1:numel(randomImgIdx)
+        name=strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/train/',D(i).name,'/',F(randomImgIdx(j)).name)
         im=double(imread(name))/255;
         singleTextureFullTestSet=[singleTextureFullTestSet im];
         size(singleTextureFullTestSet)
@@ -44,37 +45,14 @@ for i=3:numel(D)
     %singleTextureFullTestSet=[im im];
     
     % diccionario de textones
-    warning('calculate textons')
-    tic
-    [map,textons] = computeTextons(fbRun(fb,singleTextureFullTestSet),km);
-    toc
-    
-    save(strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/calculatedTextons20/map',D(i).name,'.mat'),'map') 
-    save(strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/calculatedTextons20/textons',D(i).name,'.mat'),'textons') 
+%     warning('calculate textons')
+%     tic
+%     [map,textons] = computeTextons(fbRun(fb,singleTextureFullTestSet),km);
+%     toc
+%     
+%     save(strcat('/home/jcleon/storage/disk0/Other/textures/calculatedTextons50/map',D(i).name,'.mat'),'map') 
+%     save(strcat('/home/jcleon/storage/disk0/Other/textures/calculatedTextons50/textons',D(i).name,'.mat'),'textons') 
     
 end
 
 
-%im1=double(imread('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/T09_32.jpg'))/255;
-%im = [im1 im2 im3 im4 im5 im6 im7 im8 im9 im10 ];
-
-
-sizeMap=size(map)
-sizeIM=size(im)
-%%%this size is K (clusters), bu the numel of the texton dictionary
-sizeTextons=size(textons)
-
-figure;imshow(map,[]);colormap(jet);
-
-D=dir('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/subSample/*.jpg');
-% texton maps de dos nuevas imagenes
-for i=1:numel(D),
-    im2=double(imread(strcat('/home/fuanka/Dokumente/CV/LabsCV/Lab06/textures/test/subSample/',D(i).name)))/255;
-    tmap = assignTextons(fbRun(fb,im2),textons');
-
-    % la distribucion de textones en la oveja deberia ser similar a la de la
-    % imagen de referencia.
-    figure;subplot(1,2,1);imshow(tmap,[]);colormap(jet);
-    subplot(1,2,2);bar(histc(tmap(:),1:k)/numel(tmap));
-    drawnow;
-end
